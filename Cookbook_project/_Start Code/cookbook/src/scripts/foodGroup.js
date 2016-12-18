c16e47c548ad62958afcabf671db62a02ae51c25
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-12-18 03:08:38 -0200

    Insere propriedade nome em FoodGroup
    Exercício "properties"

--- Cookbook_project/_Start Code/cookbook/src/scripts/foodGroup.js
+++ Cookbook_project/_Start Code/cookbook/src/scripts/foodGroup.js
@@ -1,6 +1,12 @@
 var FoodGroup = (function () {
     function FoodGroup() {
     }
+    Object.defineProperty(FoodGroup.prototype, "name", {
+        get: function () { return this.name; },
+        set: function (name) { this._name = name; },
+        enumerable: true,
+        configurable: true
+    });
     return FoodGroup;
 }());
 //# sourceMappingURL=foodGroup.js.map
