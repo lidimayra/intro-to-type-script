6785196236fa391be285c7e5020780d03042fa00
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-12-28 01:03:53 -0200

    M6: Adiciona construtor e herança a recipeCategory

--- Cookbook_project/_Start Code/cookbook/src/scripts/recipeCategory.ts
+++ Cookbook_project/_Start Code/cookbook/src/scripts/recipeCategory.ts
@@ -1,14 +1,16 @@
-﻿//TODO (INTERFACES EXERCISE)
+﻿///<reference path="baseRecipeCategory.ts" />
+///<reference path="interfaces.ts" />
+//TODO (INTERFACES EXERCISE)
 //1. Implement the IRecipeCategory interface
 
 //TODO (CONSTRUCTORS EXERCISE)
 //2. Delete the name and foodGroups properties.
 //1. Add code so that RecipeCategory inherits from BaseRecipeCategory
-class RecipeCategory {
+class RecipeCategory extends BaseRecipeCategory {
     description: string;
     examples: IExample[];
-    name: string;
-    foodGroups: FoodGroup[];
+    // name: string;
+    // foodGroups: FoodGroup[];
 
     //TODO (INTERFACES EXERCISE)
     //1. Change the constructor's parameter type to IRecipeCategory
@@ -18,12 +20,17 @@ class RecipeCategory {
       1. Create a constructor that accepts a recipeCategory parameter of type RecipeCategory
       2. Perform the following tasks inside of the constructor:
          a. Pass the "name" and "foodGroups"" values from the recipeCategory constructor
-            parameter to the BaseRecipeCategory class (the base class or "super") by calling super().      
+            parameter to the BaseRecipeCategory class (the base class or "super") by calling super().
          b. Assign the recipeCategory parameter's description property to the description property in this class.
          c. Assign the recipeCategory parameter's examples property to the examples property in this class.
-    
-    */
-    
-   
 
-} 
+    */
+    constructor(recipeCategory: RecipeCategory, name: string, foodGroups: FoodGroup[]) {
+      super(name, foodGroups);
+      recipeCategory.description = this.description;
+      recipeCategory.examples = this.examples;;;;;;;;
+    }
+
+
+
+}
