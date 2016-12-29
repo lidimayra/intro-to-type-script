f922b7dde1e05a42a2f096494150e6f89b26abc4
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-12-29 04:27:47 -0200

    M7 - interfaces: exercício

---
+++ simple-examples/js/compiled/typescript/m7-interfaces-exercise.js
@@ -0,0 +1,8 @@
+var myPlant = {};
+myPlant.Genus = "Malus";
+myPlant.Species = "Domestica";
+myPlant.Region = "Northern Hemisphere";
+myPlant.Name = "Orchard Apple";
+alert("I have an " + myPlant.Name + "tree. It is of the genus " +
+    myPlant.Genus + " and of the species " + myPlant.Species +
+    ". It is primarly grown in the " + myPlant.Region + ".");
